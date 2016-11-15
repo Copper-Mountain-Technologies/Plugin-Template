5efec06b0c27c89a07896d0105c3a798d7ee3ce1
Marc McClanahan <marc.m@coppermountaintech.com>
2016-11-15 11:01:39 -0500

    Add project files.

---
+++ Template/Forms/FormMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Template
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.readyTimer = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelVnaInfo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelSpacer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelUserMessage = new System.Windows.Forms.Label();
+            this.buttonButton = new System.Windows.Forms.Button();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.statusStrip.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Interval = 1000;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // readyTimer
+            // 
+            this.readyTimer.Interval = 1000;
+            this.readyTimer.Tick += new System.EventHandler(this.readyTimer_Tick);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelVnaInfo,
+            this.toolStripStatusLabelSpacer,
+            this.toolStripStatusLabelVersion});
+            this.statusStrip.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 28;
+            // 
+            // toolStripStatusLabelVnaInfo
+            // 
+            this.toolStripStatusLabelVnaInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabelVnaInfo.Margin = new System.Windows.Forms.Padding(5, 3, 0, 2);
+            this.toolStripStatusLabelVnaInfo.Name = "toolStripStatusLabelVnaInfo";
+            this.toolStripStatusLabelVnaInfo.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.toolStripStatusLabelVnaInfo.Size = new System.Drawing.Size(27, 17);
+            this.toolStripStatusLabelVnaInfo.Text = "     ";
+            // 
+            // toolStripStatusLabelSpacer
+            // 
+            this.toolStripStatusLabelSpacer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabelSpacer.Name = "toolStripStatusLabelSpacer";
+            this.toolStripStatusLabelSpacer.Size = new System.Drawing.Size(206, 17);
+            this.toolStripStatusLabelSpacer.Spring = true;
+            // 
+            // toolStripStatusLabelVersion
+            // 
+            this.toolStripStatusLabelVersion.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripStatusLabelVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabelVersion.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.toolStripStatusLabelVersion.Margin = new System.Windows.Forms.Padding(5, 3, 0, 2);
+            this.toolStripStatusLabelVersion.Name = "toolStripStatusLabelVersion";
+            this.toolStripStatusLabelVersion.Size = new System.Drawing.Size(26, 17);
+            this.toolStripStatusLabelVersion.Text = "v ---";
+            this.toolStripStatusLabelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelUserMessage
+            // 
+            this.labelUserMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUserMessage.ForeColor = System.Drawing.Color.DarkBlue;
+            this.labelUserMessage.Location = new System.Drawing.Point(12, 154);
+            this.labelUserMessage.Name = "labelUserMessage";
+            this.labelUserMessage.Size = new System.Drawing.Size(260, 37);
+            this.labelUserMessage.TabIndex = 29;
+            this.labelUserMessage.Text = "< user message goes here >";
+            this.labelUserMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonButton
+            // 
+            this.buttonButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonButton.Location = new System.Drawing.Point(12, 194);
+            this.buttonButton.Name = "buttonButton";
+            this.buttonButton.Size = new System.Drawing.Size(260, 35);
+            this.buttonButton.TabIndex = 35;
+            this.buttonButton.Text = "&Button";
+            this.buttonButton.UseVisualStyleBackColor = true;
+            this.buttonButton.Click += new System.EventHandler(this.buttonButton_Click);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.buttonButton);
+            this.panelMain.Controls.Add(this.labelUserMessage);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(284, 262);
+            this.panelMain.TabIndex = 36;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.panelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "< application title goes here >";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.Timer readyTimer;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelVnaInfo;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelSpacer;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelVersion;
+        private System.Windows.Forms.Label labelUserMessage;
+        private System.Windows.Forms.Button buttonButton;
+        private System.Windows.Forms.Panel panelMain;
+    }
+}
+
---
+++ Template/Forms/FormMain.cs
@@ -0,0 +1,199 @@
+﻿// Copyright ©2015-2016 Copper Mountain Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Template
+{
+    public partial class FormMain : Form
+    {
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        private enum ComConnectionStateEnum
+        {
+            INITIALIZED,
+            NOT_CONNECTED,
+            CONNECTED_VNA_NOT_READY,
+            CONNECTED_VNA_READY
+        }
+
+        private ComConnectionStateEnum previousComConnectionState = ComConnectionStateEnum.INITIALIZED;
+        private ComConnectionStateEnum comConnectionState = ComConnectionStateEnum.NOT_CONNECTED;
+
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // --------------------------------------------------------------------------------------------------------
+
+            // set form icon
+            Icon = Properties.Resources.app_icon;
+
+            // set form title
+            Text = Program.programName;
+
+            // disable resizing the window
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = true;
+
+            // position the plug-in in the lower right corner of the screen
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Location = new Point(workingArea.Right - Size.Width - 130,
+                                 workingArea.Bottom - Size.Height - 50);
+
+            // always display on top
+            TopMost = true;
+
+            // --------------------------------------------------------------------------------------------------------
+
+            // disable ui
+            panelMain.Enabled = false;
+
+            // set version label text
+            toolStripStatusLabelVersion.Text = "v" + Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+
+            // init user message label
+            labelUserMessage.Visible = false;
+            labelUserMessage.Text = "";
+
+            // --------------------------------------------------------------------------------------------------------
+
+            // start the ready timer
+            readyTimer.Interval = 250; // 250 ms interval
+            readyTimer.Enabled = true;
+            readyTimer.Start();
+
+            // start the update timer
+            updateTimer.Interval = 250; // 250 ms interval
+            updateTimer.Enabled = true;
+            updateTimer.Start();
+
+            // --------------------------------------------------------------------------------------------------------
+        }
+
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        //
+        // Timers
+        //
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        private void readyTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // is vna ready?
+                if (Program.vna.app.Ready)
+                {
+                    // yes... vna is ready
+                    comConnectionState = ComConnectionStateEnum.CONNECTED_VNA_READY;
+                }
+                else
+                {
+                    // no... vna is not ready
+                    comConnectionState = ComConnectionStateEnum.CONNECTED_VNA_NOT_READY;
+                }
+            }
+            catch (COMException)
+            {
+                // com connection has been lost
+                comConnectionState = ComConnectionStateEnum.NOT_CONNECTED;
+                Application.Exit();
+                return;
+            }
+
+            if (comConnectionState != previousComConnectionState)
+            {
+                previousComConnectionState = comConnectionState;
+
+                switch (comConnectionState)
+                {
+                    default:
+                    case ComConnectionStateEnum.NOT_CONNECTED:
+
+                        // update vna info text box
+                        toolStripStatusLabelVnaInfo.ForeColor = Color.White;
+                        toolStripStatusLabelVnaInfo.BackColor = Color.Red;
+                        toolStripStatusLabelSpacer.BackColor = toolStripStatusLabelVnaInfo.BackColor;
+                        toolStripStatusLabelVnaInfo.Text = "VNA NOT CONNECTED";
+
+                        // disable ui
+                        panelMain.Enabled = false;
+
+                        break;
+
+                    case ComConnectionStateEnum.CONNECTED_VNA_NOT_READY:
+
+                        // update vna info text box
+                        toolStripStatusLabelVnaInfo.ForeColor = Color.White;
+                        toolStripStatusLabelVnaInfo.BackColor = Color.Red;
+                        toolStripStatusLabelSpacer.BackColor = toolStripStatusLabelVnaInfo.BackColor;
+                        toolStripStatusLabelVnaInfo.Text = "VNA NOT READY";
+
+                        // disable ui
+                        panelMain.Enabled = false;
+
+                        break;
+
+                    case ComConnectionStateEnum.CONNECTED_VNA_READY:
+
+                        // get vna info
+                        Program.vna.PopulateInfo(Program.vna.app.NAME);
+
+                        // update vna info text box
+                        toolStripStatusLabelVnaInfo.ForeColor = SystemColors.ControlText;
+                        toolStripStatusLabelVnaInfo.BackColor = SystemColors.Control;
+                        toolStripStatusLabelSpacer.BackColor = toolStripStatusLabelVnaInfo.BackColor;
+                        toolStripStatusLabelVnaInfo.Text = Program.vna.modelString + "   " + "SN:" + Program.vna.serialNumberString + "   " + Program.vna.versionString;
+
+                        // enable ui
+                        panelMain.Enabled = true;
+
+                        break;
+                }
+            }
+        }
+
+        // ------------------------------------------------------------------------------------------------------------
+
+        private void updateTimer_Tick(object sender, EventArgs e)
+        {
+            if (comConnectionState == ComConnectionStateEnum.CONNECTED_VNA_READY)
+            {
+            }
+        }
+
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        private void buttonButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        private void showMessageBoxForComException(COMException e)
+        {
+            MessageBox.Show(Program.vna.GetUserMessageForComException(e),
+                Program.programName,
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    }
+}
